ea416a976093a1dab7a12a51bf8ca42cda6d3c76
Frankley Kaiky <kaikym20@gmail.com>
2022-04-22 15:50:06 -0300

    Att

--- Udemy/Day7/Hangman.py
+++ Udemy/Day7/Hangman.py
@@ -1,11 +1,31 @@
-from random import *
+import random
+from words import words
+from hang_visual import lives_visual_dict
+
+def app(lista, list2):
+    for c in range(0, len(list2)):
+        lista.append('_')
 
 while True:
-    index= randint(0, 300)
-    file= open("palavras.txt", 'rt')
-    l= file.readlines()
-    selected= l[index].strip().split('\n')
-    print(selected)
-    n=int(input('Num: '))
-    if n==1:
+    life=6
+    word= random.choice(words).upper()
+    while '-' in word or ' ' in word:
+        word= random.choice(words).upper()
+    letters= []
+    for l in word:
+        letters.append(l)
+    print(word)
+    tabe=[]
+    app(tabe, letters)
+    print(tabe)
+    while life>0:
+        guess= str(input("Guess a letter: ")).upper()
+        if guess in letters:
+            pos= letters.index(guess)
+            tabe[pos]= guess
+            print(tabe)
+        else:
+            print(False)
+    num= int(input('NUM: '))
+    if num==0:
         break
