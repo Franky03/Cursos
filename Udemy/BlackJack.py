e965f7aa4b6e0714e2724fa1ff495c1f521e52e7
Frankley Kaiky <kaikym20@gmail.com>
2022-04-27 00:02:09 -0300

    BlackJack update

--- Udemy/BlackJack.py
+++ Udemy/BlackJack.py
@@ -1,73 +1,103 @@
-import random 
-playerIn= True
-dealerIn=True
-deck=[2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,'J','Q','K','A','J','Q','K','A','J','Q','K','A','J','Q','K','A',]
-playerHand= []
-dealerHand=[]
+import random
+from time import sleep
 
-def dealCard(turn):
-    card= random.choice(deck)
+#Functions
+
+def card(turn):
+    card= random.choice(baralho)
     turn.append(card)
-    deck.remove(card)
+    baralho.remove(card)
 
 def total(turn):
     total=0
-    face=['J','K','Q']
+    face=['Q', 'K', 'J']
     for card in turn:
         if card in range(1,11):
-            total+=card
+            total += card
         elif card in face:
-            total +=10
+            total += 10
         else:
             if total>11:
-                total+=1
+                total +=1
             else:
-                total+=11
+                total +=11 
+    if total==21 and len(turn)==2:
+        return 0
     return total
 
-def revealDealerHand():
-    if len(dealerHand)==2:
-        return dealerHand[0]
-    elif len(dealerHand)>2:
-        return dealerHand[0],[1]
-
-for _ in range(2):
-    dealCard(dealerHand)
-    dealCard(playerHand)
-
-while playerIn or dealerIn:
-    print(f"Dealer had {revealDealerHand()} and X")
-    print(f"You have {playerHand} for a total of {total(playerHand)}.")
-    if playerIn:
-        stayOrHit= input("1: Stay\n2: Hit\n")
-    if total(dealerHand)>16:
-        dealerIn=False
+def compare(score_p, score_d):
+    if score_p==score_d:
+        return 'Draw!'
+    elif score_p==0:
+        return "You Win with a BlackJack!"
+    elif score_d==0:
+        return 'You Lose, dealer has a BlackJack!'
+    elif score_p>21:
+        return 'You went over ,You Lose!'
+    elif score_d>21:
+        return 'Dealer went over, You Win!'
+    elif score_p> score_d:
+        return "You Win!"
     else:
-        dealCard(dealerHand)
-    if stayOrHit=='1':
-        playerIn==False
-    else:
-        dealCard(playerHand)
-    if total(playerHand)>=21:
-        break
-    elif total(dealerHand)>=21:
-        break
+        return "You Lose!"
 
-if total(playerHand)==21:
-    print(f"\nYou have {playerHand} for a total of {total(playerHand)} and {dealerHand} for a total of {total(dealerHand)}")
-    print("BlackJack! You Win!")
-elif total(dealerHand)==21:
-    print(f"\nYou have {playerHand} for a total of {total(playerHand)} and {dealerHand} for a total of {total(dealerHand)}")
-    print("BlackJack! Dealer wins")
-elif total(playerHand)>21:
-    print(f"\nYou have {playerHand} for a total of {total(playerHand)} and {dealerHand} for a total of {total(dealerHand)}")
-    print("You bust! Dealer Wins!")
-elif total(dealerHand)>21:
-    print(f"\nYou have {playerHand} for a total of {total(playerHand)} and {dealerHand} for a total of {total(dealerHand)}")
-    print("Dealer bust! You win!")
-elif 21- total(dealerHand)< 21- total(playerHand):
-    print(f"\nYou have {playerHand} for a total of {total(playerHand)} and {dealerHand} for a total of {total(dealerHand)}")
-    print("Dealer Wins !")
-elif 21-total(dealerHand)> 21- total(playerHand):
-    print(f"\nYou have {playerHand} for a total of {total(playerHand)} and {dealerHand} for a total of {total(dealerHand)}")
-    print("You Win !")
+def emb(text):
+    print(text, end='')
+    for c in range(0,4):
+        sleep(0.5)
+        print('.', end='', flush=True)
+        sleep(0.5)
+    print()
+
+#Principal Program
+
+while True:
+    baralho=[2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,'J','Q','K','A','J','Q','K','A','J','Q','K','A','J','Q','K','A',]
+    dealer= []
+    player=[]
+    game_over=False
+    for _ in range(2):
+        card(player)
+        card(dealer)
+    while True:
+        score_p= total(player)
+        score_d= total(dealer)
+        print("--"*20)
+        print(f" Your cards: {player}, current score: {score_p}")
+        print(f" Dealer's firt card: {dealer[0]}")
+        print("--"*20)
+        if score_p==0 or score_d==0 or score_p>21:
+            game_over=True
+        else:
+            while True:
+                user_choice= str(input("[Y]Get Another Card\n[N]Pass\nChoice: ")).upper().strip()
+                if user_choice=='Y' or user_choice=='N':
+                    break
+                else:
+                    print('ERROR')
+            print("--"*20)
+            if user_choice=='Y':
+                emb('Shuffling')
+                card(player)
+            else:
+                game_over= True
+        if game_over:
+            break
+    while score_d !=0 and score_d < 17:
+        card(dealer)
+        score_d= total(dealer)
+    print(f"You have: {player} For a total of: {score_p}\nDealer have: {dealer} For a total of: {score_d}!")
+    print(compare(score_p, score_d))
+    resp=''
+    while True:
+        resp= str(input("Want to play again? [Y/N]: ")).upper().strip()[0]
+        if resp=='Y' or resp=='N':
+            break
+        else:
+            print("ERROR")
+    if resp=='N':
+        print("END")
+        break
+    else:
+        player.clear()
+        dealer.clear()
