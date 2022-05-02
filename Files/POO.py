976bd67a3f40bb04b99fcf9719e2f1f5d0b6fec0
Frankley Kaiky <kaikym20@gmail.com>
2022-05-02 16:28:14 -0300

    Update POO.py

--- Files/POO.py
+++ Files/POO.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from random import *
+from matplotlib.pyplot import table
 from prettytable import PrettyTable
 
 class Pessoa:
@@ -121,6 +122,7 @@ p2= Produto('CANECA', 'R$15')
 print(p2.preco)
 print(p2.nome)
 
+#Atributos de classe
 class A:
     vc=123
     def __init__(self):
@@ -137,6 +139,7 @@ class A:
 # print(a2.vc)
 # print(A.vc)
 
+#Encapsulamento
 class BaseDeDado:
     def __init__(self):
         self.__dados= {}
@@ -155,10 +158,172 @@ class BaseDeDado:
     def remover_cliente(self, id):
         del self.__dados['clientes'][id]
 
-bd= BaseDeDado()
-bd.inserir_cliente(1, 'Otávio')
-bd.inserir_cliente(2, 'Rose')
-bd.inserir_cliente(3, 'Marcelo')
-bd.inserir_cliente(4, 'Julio')
-bd.remover_cliente(3)
-bd.lista_clientes()
+# bd= BaseDeDado()
+# bd.inserir_cliente(1, 'Otávio')
+# bd.inserir_cliente(2, 'Rose')
+# bd.inserir_cliente(3, 'Marcelo')
+# bd.inserir_cliente(4, 'Julio')
+# bd.remover_cliente(3)
+# bd.lista_clientes()
+
+#Associação de classes
+class Escritor:
+    def __init__(self, nome):
+        self.__nome= nome
+        self.__ferramenta= None
+
+    @property
+    def nome(self):
+        return self.__nome
+    @property
+    def ferramenta(self):
+        return self.__ferramenta
+    
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self.__ferramenta= ferramenta
+
+class Caneta:
+    def __init__(self, marca):
+        self.__marca= marca
+    
+    @property
+    def marca(self):
+        return self.__marca
+
+    def escrever(self):
+        print("Caneta está escrevendo...")
+    
+class MaquinaDeEscrever:
+    def escrever(self):
+        print("Máquina está escrevendo...") 
+
+
+escritor= Escritor('Otávio')
+caneta= Caneta('Bic')
+maquina= MaquinaDeEscrever()
+
+escritor.ferramenta= caneta
+escritor.ferramenta.escrever()
+print(escritor.ferramenta.marca) 
+caneta.escrever()
+
+#Agregação
+
+class Carrinho:
+    def __init__(self):
+        self.produtos=[]
+
+    def inserir(self, produto):
+        self.produtos.append(produto)
+    
+    def lista_produto(self):
+        table= PrettyTable()
+        table.field_names= ['Produtos', 'Preço']
+        for p in self.produtos:
+            table.add_row([p.nome, p.valor])
+        total=0
+        for produto in self.produtos:
+            total+=produto.valor
+        table.add_row(['TOTAL' ,total])
+        print(table)
+       
+class Produto:
+    def __init__(self, nome, valor):
+        self.nome= nome
+        self.valor= valor
+
+# carrinho= Carrinho()
+# p1= Produto('Playstation', 1549)
+# p2= Produto('Iphone', 6890)
+# p3= Produto('Camiseta', 56)
+# p4= Produto('Cabo USB', 35)
+# carrinho.inserir(p2)
+# carrinho.inserir(p1)
+# carrinho.inserir(p3)
+# carrinho.inserir(p4)
+# carrinho.lista_produto()
+
+#Composição
+
+class Cliente:
+    def __init__(self, nome, idade):
+        self.nome= nome
+        self.idade= idade
+        self.enderecos= []
+    def insere_endereco(self, cidade, estado):
+        self.enderecos.append(Endereco(cidade, estado)) #Usou a classe endereço para compor um cliente na classe Cliente
+
+    def lista_enderecos(self):
+        for endereco in self.enderecos:
+            print(endereco.cidade, endereco.estado) 
+    def __del__(self):
+        print(f'{self.nome} FOI APAGADO')
+
+
+
+class Endereco:
+    def __init__(self, cidade, estado):
+        self.cidade= cidade
+        self.estado= estado
+    def __del__(self):
+        print(f'{self.cidade}/{self.estado} FOI APAGADO')
+
+
+# c1= Cliente('Luiz', 32)
+# c1.insere_endereco('Belo Horizonte', 'MG')
+# print(c1.nome)
+# c1.lista_enderecos()
+# # del c1
+# print()
+
+# c2= Cliente('Maria', 55)
+# c2.insere_endereco('Salvador', 'BA')
+# c2.insere_endereco('Rio de Janeiro', 'RJ')
+# print(c2.nome)
+# c2.lista_enderecos()
+# # del c2
+# print()
+
+# c3= Cliente('João', 19)
+# c3.insere_endereco('São Paulo', 'SP')
+# print(c3.nome)
+# c3.lista_enderecos()
+# # del c3
+# print('___________________')
+
+#VEJA A DIFERENÇA DEPOIS DESSA LINHA COM O DEL C1, DEL C2 E DEL C3
+#Quando a classe cliente é criada e cria uma classe endereço, assim que ela for apagada, a classe endereço também será apagada da memória
+#Quando uma morrer a outra também morre
+
+#Herança simples
+class Pessoa: #SuperClasses
+    def __init__(self, nome, idade):
+        self.nome= nome
+        self.idade=idade
+        self.nomeclasse= self.__class__.__name__
+
+    def falar(self):
+        print(f'{self.nomeclasse} falando...')
+#SubClasses
+class Client(Pessoa): #Um cliente é uma pessoa, ele herda o que tem dentro de pessoa
+    
+    def comprar(self):
+        print(f'{self.nomeclasse} comprando...')
+
+class Aluno(Pessoa):
+    def estudar(self):
+        print(f'{self.nomeclasse} estudando...')
+
+c1= Client('Luiz', 45)
+c2= Aluno('Jão', 15)
+print(c1.nome)
+c1.falar()
+c1.comprar()
+#c1.estudar() dá errado pq não existe no c1
+print(c2.nome)
+c2.falar()
+c2.estudar()
+
+p1= Pessoa('Maria', 50)
+p1.falar()
